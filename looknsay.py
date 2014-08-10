cd67e3d2419fd84fd11e469d17562bba19131099
Marek Kaput <jjkbtv@gmail.com>
2014-08-10 16:41:40 +0200

    Add option to print only last step of iteration.

--- looknsay.py
+++ looknsay.py
@@ -20,14 +20,27 @@ def compute(n, i=20):
         yield x
 
 
+def last_item(iter):
+    item = None
+    for item in iter:
+        pass
+    return item
+
+
 if __name__ == "__main__":
     parser = ArgumentParser(description='Generate sequence of Look-and-Say numbers.')
     parser.add_argument('seed', type=int, nargs='?', default=1,
                         help='sequence seed')
     parser.add_argument('-i', '--iterations', dest='iterations', type=int, default=20,
                         help='number of iterations')
+    parser.add_argument('-last', '--only-last', action='store_true',
+                        help='print only last step of the iteration')
 
     args = parser.parse_args()
+    computer = compute(args.seed, args.iterations)
 
-    for step in compute(args.seed, args.iterations):
-        print(step)
+    if not args.only_last:
+        for step in computer:
+            print(step)
+    else:
+        print(last_item(computer))
